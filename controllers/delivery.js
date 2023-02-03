8cb5ac96cef2a9a3804211a968f0febbce58028b
JuanPabloRiglos <juan.riglos@hotmail.com>
2023-02-03 10:14:18 -0300

    funcion edit producto delivery, vista detalle de producto y boton de edit listo

--- controllers/delivery.js
+++ controllers/delivery.js
@@ -8,10 +8,12 @@ let deliveryController = {
     ir : function(req, res){
         res.render('delivery/deliveryInicio.ejs') 
     },
-    cargarProducto : function(req,res){// !! muestra la pagina de registro de carga de producto por get.
+    //////////////////////////////////////////////////// !! muestra la pagina de registro de carga de producto por get.
+    cargarProducto : function(req,res){
        res.render('delivery/registroDelivery')
     },
-    guardarProducto : function(req, res){ // !! guarda los datos del producto creado.
+    guardarProducto : function(req, res){ 
+        /////////////////////////////////////////////// !! guarda los datos del producto creado.
         //genero id de producto
         let id ;
         if(productosEnDB.length == [] ){
@@ -43,6 +45,37 @@ let deliveryController = {
        
         
         res.redirect('/')
+    },
+    showProducts: function(req, res){
+        ///////////////////////////////// Muestra los productos en db
+        //res.render(productosEnDB)
+        res.render('delivery/dProducts', {productosEnDB})
+       
+    },
+    ProductDetail: function(req, res){
+        //////////////////////////////////// Muestra la vista con el detalle de producto y permite cambiar datos 
+        let idBuscado = req.params.id;
+        let productoDetalle = productosEnDB.find(producto => producto.id == idBuscado);
+        res.render('delivery/dProductDetail', {product : productoDetalle})
+    }, 
+    editProduct: function(req, res){
+        //////////////////////////////////// Guarda los datos editados previamente
+        let idBuscado = Number(req.params.id);
+       
+        let editedProduct = {
+            id : idBuscado ,
+            ...req.body,
+            //trae todo lo que se envio en el formulario y lo guarda
+        };
+        const updateProduct = productosEnDB.map((p) => {
+            if(p.id == idBuscado){
+                return ( p = {...editedProduct})
+            }else{
+                return p
+            }
+        })
+        fs.writeFileSync(productosFilePath, JSON.stringify( updateProduct,'utf-8'));
+        res.render('delivery/dProductDetail', {product : editedProduct})
     }
 }
 module.exports = deliveryController;
--- routes/delivery.js
+++ routes/delivery.js
@@ -5,5 +5,7 @@ let deliveryController = require('../controllers/delivery');
 router.get('/', deliveryController.ir);
 router.get('/registroDelivery', deliveryController.cargarProducto);
 router.post('/registroDelivery', deliveryController.guardarProducto);
-
+router.get('/dProducts', deliveryController.showProducts);
+router.get('/dProductDetail/:id', deliveryController.ProductDetail);
+router.patch('/dProductDetail/:id', deliveryController.editProduct);
 module.exports = router;
